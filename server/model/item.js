bedb961865af79a24707af4092fe2e3112228229
Happy Hiten <hiten.diwan@ontariotechu.net>
2024-11-26 20:06:07 -0500

    Updates
    Updating the item.js

--- server/model/item.js
+++ server/model/item.js
@@ -1,17 +1,18 @@
-//const { Collection, default: mongoose } = require("mongoose");
+const mongoose = require('mongoose');
 
-const mongoose = require("mongoose");
-
-let inventoryModel = mongoose.Schema({
+let inventoryModel = mongoose.Schema(
+  {
     ItemName: String,
     Supplier: String,
     DateReceived: Date,
     ItemDescription: String,
     Cost: Number,
-    Quantity: Number
-},
-{
-    collection: "Warehouse_Inventory"
-});
-module.exports = mongoose.model('Item', inventoryModel);
+    Quantity: Number,
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }, // Reference to User model
+  },
+  {
+    collection: 'Warehouse_Inventory',
+  }
+);
 
+module.exports = mongoose.model('Item', inventoryModel);
